47adba4709a47f3effd508efa076ce3c29c4db84
unknown <vinhphat493@gmail.com>
2018-11-22 02:37:36 +0700

    Hoan thanh Content phim dang chieu chua reponsive

--- LayoutCinema/js/main.js
+++ LayoutCinema/js/main.js
@@ -4,12 +4,12 @@
  * Nội dung : xử lý tính năng giao diện
  */
 
-$(document).ready(()=>{
+$(document).ready(() => {
     //Xử lý hiệu ứng nút search
     $(".searchBox").hide();
-    $(".header__nav-btnSearch").on('click',()=>{
+    $(".header__nav-btnSearch").on('click', () => {
         $(".searchBox").slideToggle();
-        if($(".header__nav-list").hasClass("active-list")){
+        if ($(".header__nav-list").hasClass("active-list")) {
             $(".header__nav-list").toggleClass("active-list");
             $(".header__nav-btnNav").toggleClass("active-fa");
         }
@@ -17,18 +17,48 @@ $(document).ready(()=>{
 
     var a = true;
     //Xử lý responsive menu
-    $(".header__nav-btnNav").on('click',()=>{
-        if(a==true){
+    $(".header__nav-btnNav").on('click', () => {
+        if (a == true) {
             $("body").addClass("active-body");
             $(".header__nav-list").addClass("active-list");
             $(".header__nav-btnNav").addClass("active-fa");
             $(".searchBox").fadeOut();
-            a=false;
-        }else{
+            a = false;
+        } else {
             $("body").removeClass("active-body");
             $(".header__nav-list").removeClass("active-list");
             $(".header__nav-btnNav").removeClass("active-fa");
-            a=true;
+            a = true;
         }
+    })
+
+    // Tạo carousel home
+    $('.carousel__home').slick({
+        dots: false,
+        fade: true,
+        cssEase: 'linear',
+        draggable : false,
+        asNavFor: ".carousel__intro-item"
+    });
+
+    // Tạo carousel new movie
+    $('.carousel__intro-item').slick({
+        slidesToShow: 4, 
+        slidesToScroll: 1,
+        draggable : false,
+        asNavFor: ".carousel__home",
+        arrows : true,
+    })
+
+    //Ẩn thanh trượt
+    $(".slick-next").addClass("d-none");
+    $(".slick-prev").addClass("d-none");
+
+    //Triger thanh trượt
+    $("#btn-next").on('click',()=>{
+        $(".slick-next").trigger('click');
+    })
+    $("#btn-prev").on('click',()=>{
+        $(".slick-prev").trigger('click');
     })
 })
